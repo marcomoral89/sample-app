6ad470a7e559da46f6c4f35b388675c2e2495a3b
marcomoral89 <marco.moral89@gmail.com>
2021-10-28 14:44:06 -0700

    add post method in index.html and authcodeflow.html

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const basicAuth = require('express-basic-auth');
 const port = 3000;
 const bodyParser = require('body-parser');
 var thinkificSub = '';
-var accessToken = '';
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+// ENV VARIABLES
 require('dotenv').config();
 process.env.USER_ID;
 process.env.USER_KEY;
@@ -18,19 +20,26 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.post('/', (req, res) => {
+  var installSub = req.body.subdomain.split('.');
+  res.redirect(`http://localhost:3000/install?subdomain=${installSub[0]}`);
+});
+
 // INSTALL URL
 app.get('/install', (req, res) => {
   const subdomain = req.query.subdomain;
+  // thinkificSub = `https://${subdomain}.thinkific.com`;
   thinkificSub = `https://${subdomain}.thinkific.com`;
+
   const redirect_uri = `http://localhost:${port}/authcodeflow`;
 
-  // REDIRECT USER TO AUTH URL
+  // REDIRECT USER TO AUTH PAGE
   res.redirect(
     `https://${subdomain}.thinkific.com/oauth2/authorize?client_id=${process.env.USER_ID}&redirect_uri=${redirect_uri}&response_mode=query&response_type=code&scope=write:site_scripts`
   );
 });
 
-// REDIRECT URI/CALL BACK URL TO RETRIEVE CCESS TOKEN
+// REDIRECT URI/CALL BACK URL TO RETRIEVE ACCESS TOKEN
 app.get('/authcodeflow', (req, res) => {
   // BODY PARAMETERS
   const json = JSON.stringify({
@@ -53,19 +62,35 @@ app.get('/authcodeflow', (req, res) => {
     })
     .then((response) => {
       process.env.ACCESS_TOKEN = response.data.access_token;
-      res.send('Access Token = ' + 'Bearer ' + process.env.ACCESS_TOKEN);
+      // console.log(process.env.ACCESS_TOKEN);
+      // console.log(response.data);
+      res.sendFile(__dirname + '/views/authcodeflow.html');
     })
     .catch((error) => res.send(error));
 });
 
-// app.get('/integrations', (req, res) => {
-//   res.sendFile(__dirname + '/views/integrations.html');
-// });
+// API REQUEST ON POST
+app.post('/authcodeflow', (req, res) => {
+  var method = req.body.method;
+  var baseUrl = req.body.baseUrl;
 
-// app.post('/integrations', (req, res) => {
-//   console.log(req.body.authFlow);
-//   res.send('thank you for the post');
-// });
+  axios({
+    method: method,
+    url: baseUrl,
+    headers: {
+      Authorization: 'Bearer ' + process.env.ACCESS_TOKEN,
+      'Content-Type': 'application/json',
+    },
+    // data: {
+    //   firstName: 'Finn',
+    //   lastName: 'Williams',
+    // },
+  })
+    .then((response) => {
+      res.send(response.data);
+    })
+    .catch((error) => res.send(error));
+});
 
 // SERVER PORT
 app.listen(port, () => {
